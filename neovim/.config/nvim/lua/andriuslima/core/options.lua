2316017181f35944ffa1ff9ae3346068c9c71154
Andrius Lima <andriuslima@icloud.com>
2023-04-26 23:26:51 -0300

    Refactoring neovim configs

--- neovim/.config/nvim/lua/andriuslima/core/options.lua
+++ neovim/.config/nvim/lua/andriuslima/core/options.lua
@@ -1,40 +1,74 @@
-local options = {
-	visualbell = true,
-	clipboard = "unnamedplus",
-	cmdheight = 3,
-	completeopt = { "menuone", "noselect" },
-	fileencoding = "utf-8",
-	hlsearch = false,
-	ignorecase = true,
-	mouse = "a",
-	pumheight = 10,
-	showmode = false,
-	showtabline = 2,
-	smartcase = true,
-	smartindent = true,
-	splitbelow = true,
-	splitright = true,
-	swapfile = false,
-	termguicolors = true,
-	timeoutlen = 100,
-	undofile = true,
-	updatetime = 300,
-	writebackup = false,
-	expandtab = true,
-	shiftwidth = 4,
-	tabstop = 4,
-	cursorline = true,
-	number = true,
-	relativenumber = true,
-	numberwidth = 1,
-	signcolumn = "yes",
-	wrap = false,
-	scrolloff = 8,
-	sidescrolloff = 8,
-}
+vim.opt.visualbell = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 3
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.fileencoding = "utf-8"
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.mouse = "a"
+vim.opt.pumheight = 10
+vim.opt.showmode = false
+vim.opt.showtabline = 2
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 100
+vim.opt.undofile = true
+vim.opt.updatetime = 300
+vim.opt.writebackup = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 1
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
 
-vim.opt.shortmess:append("c")
+--[[ local options = { ]]
+--[[ 	visualbell = true, ]]
+--[[ 	clipboard = "unnamedplus", ]]
+--[[ 	cmdheight = 3, ]]
+--[[ 	completeopt = { "menuone", "noselect" }, ]]
+--[[ 	fileencoding = "utf-8", ]]
+--[[ 	hlsearch = false, ]]
+--[[ 	ignorecase = true, ]]
+--[[ 	mouse = "a", ]]
+--[[ 	pumheight = 10, ]]
+--[[ 	showmode = false, ]]
+--[[ 	showtabline = 2, ]]
+--[[ 	smartcase = true, ]]
+--[[ 	smartindent = true, ]]
+--[[ 	splitbelow = true, ]]
+--[[ 	splitright = true, ]]
+--[[ 	swapfile = false, ]]
+--[[ 	termguicolors = true, ]]
+--[[ 	timeoutlen = 100, ]]
+--[[ 	undofile = true, ]]
+--[[ 	updatetime = 300, ]]
+--[[ 	writebackup = false, ]]
+--[[ 	expandtab = true, ]]
+--[[ 	shiftwidth = 4, ]]
+--[[ 	tabstop = 4, ]]
+--[[ 	cursorline = true, ]]
+--[[ 	number = true, ]]
+--[[ 	relativenumber = true, ]]
+--[[ 	numberwidth = 1, ]]
+--[[ 	signcolumn = "yes", ]]
+--[[ 	wrap = false, ]]
+--[[ 	scrolloff = 8, ]]
+--[[ 	sidescrolloff = 8, ]]
+--[[ } ]]
+--[[]]
+--[[ vim.opt.shortmess:append("c") ]]
+--[[]]
+--[[ for k, v in pairs(options) do ]]
+--[[ 	vim.opt[k] = v ]]
+--[[ end ]]
 
-for k, v in pairs(options) do
-	vim.opt[k] = v
-end
