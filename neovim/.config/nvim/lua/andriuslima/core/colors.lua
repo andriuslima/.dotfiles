ae4da0746714df77e3bf786991e230a66ebc5abd
Andrius Lima <andriuslima@icloud.com>
2023-04-26 23:26:32 -0300

    Migrating neovim colorscheme from github to moonfly

--- neovim/.config/nvim/lua/andriuslima/core/colors.lua
+++
@@ -1,9 +0,0 @@
-local github_theme_status_ok, github_theme = pcall(require, "github-theme") -- Protective call to load theme
-
-if not github_theme_status_ok then
-    print("Could not load github-theme")
-    return
-end
-
-vim.cmd "colorscheme moonfly"
-vim.cmd "set colorcolumn=120"
--- neovim/.config/nvim/lua/andriuslima/core/init.lua
+++ neovim/.config/nvim/lua/andriuslima/core/init.lua
@@ -1,5 +1,5 @@
 print("Loading core settings")
 
-require("andriuslima.core.colors")
 require("andriuslima.core.options")
 require("andriuslima.core.keymaps")
+require("andriuslima.core.appearance")
